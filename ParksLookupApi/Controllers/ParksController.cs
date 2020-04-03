4d66c1a6d1fd5e8f3761e6b81bd5c7f3d9121e87
Jieun Kang <jieunkang101@gmail.com>
2020-04-03 10:29:59 -0700

    add POST, PUT and DELETE methods in ParksController.cs

--- ParksLookupApi/Controllers/ParksController.cs
+++ ParksLookupApi/Controllers/ParksController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ParksLookupApi.Models;
 
 namespace ParksLookupApi.Controllers
@@ -30,6 +31,30 @@ namespace ParksLookupApi.Controllers
       return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
     }
 
+    // POST api/parks
+    [HttpPost]
+    public void Post([FromBody] Park park)
+    {
+      _db.Parks.Add(park);
+      _db.SaveChanges();
+    }
 
+    // PUT api/parks/3
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Park park)
+    {
+      park.ParkId = id;
+      _db.Entry(park).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/parks/3
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+      _db.Parks.Remove(parkToDelete);
+      _db.SaveChanges();
+    }
   }
 }
