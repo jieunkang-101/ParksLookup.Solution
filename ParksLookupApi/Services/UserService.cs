a70b2ce8244c4b4b94a03583cc2c039cdf5308be
Jieun Kang <jieunkang101@gmail.com>
2020-04-03 18:30:07 -0700

    update UserService.cs to fix authorization error

--- ParksLookupApi/Services/UserService.cs
+++ ParksLookupApi/Services/UserService.cs
@@ -52,7 +52,8 @@ namespace ParksLookupApi.Services
       {
         Subject = new ClaimsIdentity(new Claim[] 
         {
-          new Claim(ClaimTypes.Name, user.Id.ToString())
+          new Claim(ClaimTypes.Name, user.Id.ToString()),
+          new Claim(ClaimTypes.Role, user.Role)
         }),
         Expires = DateTime.UtcNow.AddDays(7),
         SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
